30375d5c499dc4e58cb8f44fff654d48f797ab57
Kastier Francisco Leite Júnior <108149154+kastjr@users.noreply.github.com>
2022-12-19 17:45:05 -0300

    Add files via upload

---
+++ aula04/ferramentas.js
@@ -0,0 +1,17 @@
+
+// AULA 06
+s.length //conta a quantidade de caracteres da string ou de um vetor
+s.toUpperCase() //transforma em maiuscula
+s.LowerCase() //coloca tudo em minuscula
+s.map() //mapeia o vetor ou o arquivo e transforrma no que for escolhido
+s.split() //divide ou separa onde for selecionado
+document.write //escreva no documento
+document.writeln // pula linha 
+s.ToFixed() //escolhe a qtd de casas decimais que vc quer mostrar
+s.ToFixed(2).replace('.',',') // troca o . por virgula
+Number.parseInt() //TRANSFORMA EM NUMEROM INTEIRO
+Number.parseFloat() //transforma em numero decimal
+n1.toLocaleString('pt-BR',{style: 'currency', currency: 'BRL'})
+n1.toLocaleString('pt-BR',{style: 'currency', currency: 'USD'})
+n1.toLocaleString('pt-BR',{style: 'currency', currency: 'EUR'})
+
