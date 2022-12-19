43d27ccf8b79908ffdf729a4408964226e7bf893
Kastier Francisco Leite Júnior <108149154+kastjr@users.noreply.github.com>
2022-12-19 17:44:42 -0300

    Delete guanabara javascript/aula04 directory

--- guanabara javascript/aula04/ferramentas.js
+++
@@ -1,9 +0,0 @@
-
-s.length //conta a quantidade de caracteres da string ou de um vetor
-s.toUpperCase() //transforma em maiuscula
-s.LowerCase() //coloca tudo em minuscula
-s.map() //mapeia o vetor ou o arquivo e transforrma no que for escolhido
-s.split() //divide ou separa onde for selecionado
-document.write //escreva no documento
-document.writeln // pula linha 
-s.ToFixed() //escolhe a qtd de casas decimais que vc quer mostrar
