04b05e943f07e7db76f4f12aaa5e5ee5f2b58398
Kastier Francisco Leite Júnior <108149154+kastjr@users.noreply.github.com>
2022-12-26 13:47:17 -0300

    Add files via upload

---
+++ aula11/ex008.js
@@ -0,0 +1,5 @@
+var vel = 60
+console.log(`a velocidade do seu carro é ${vel}km/h`)
+if(vel > 60) {
+    console.log('multado')
+}
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'eua'
+console.log(`vivendo em ${pais}`)
+if (pais == 'br') {
+    console.log('brasileiro')
+} else {
+    console.log('estrangeiro')
+}
