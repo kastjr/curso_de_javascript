10b571b5f0cf836b1c549758ca11c20c071a17be
Kastier Francisco Leite Júnior <108149154+kastjr@users.noreply.github.com>
2022-12-27 18:55:51 -0300

    Add files via upload

---
+++ aula12/ex011.js
@@ -0,0 +1,10 @@
+var idade = 22
+if (idade < 16) {
+    console.log('nao vota')
+} else if (idade >= 16 && idade < 18) {
+    
+        console.log('voto opcional')
+    
+} else {
+    console.log('voto origatorio')
+}
